fbeca613d5b67353b3ea5fecd1f302f8a96643f6
pdolawat654 <36255701+pdolawat654@users.noreply.github.com>
2020-06-12 21:49:38 +0530

    Deleted all

--- accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from accounts.models import Patient,Doctor
-
-admin.site.register(Patient)
-admin.site.register(Doctor)
--- prescriptions/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Prescription
-
-admin.site.register(Prescription)
